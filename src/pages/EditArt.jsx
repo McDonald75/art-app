bad0e7e6af5f6b85706a3f3602a661bfdc69ba80
McDonald75 <mcdonald.omondi@student.moringaschool.com>
2024-11-15 10:55:40 +0300

    updated edit page

--- src/pages/EditArt.jsx
+++ src/pages/EditArt.jsx
@@ -25,35 +25,37 @@ const EditArt = () => {
   const handleUpdate = (e) => {
     e.preventDefault();
 
-    
-    const updatedArts = arts.map((art) =>
-      art.id === parseInt(id) ? { ...art, name, author, price, imageLink } : art
-    );
-
-    setArts(updatedArts);
-    navigate('/'); 
   };
 
   return (
     <div className="edit-art-container">
       <h2>Edit Artwork</h2>
       <form onSubmit={handleUpdate}>
-        <label>
-          Name:
+        <div>
+          <p>Name</p>
           <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
-        </label>
-        <label>
+        </div>
+        <div>
+          <p>
           Author:
+
+          </p>
           <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} required />
-        </label>
-        <label>
+        </div>
+        <div>
+          <p>
           Price:
+
+          </p>
           <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} required />
-        </label>
-        <label>
+        </div>
+        <div>
+          <p>
           Image Link:
+
+          </p>
           <input type="url" value={imageLink} onChange={(e) => setImageLink(e.target.value)} required />
-        </label>
+        </div>
         <button type="submit">Update Artwork</button>
       </form>
     </div>
